8b7b5c3c74bc305a93ca8305e36f0f47b6309f4d
gyojincompany <luckyred@gmail.com>
2024-07-02 15:00:23 +0900

    스프링부트 기본 환경설정 및 jsp 설정 추가

---
+++ src/main/java/com/gyojincompany/test/SpringbootTest120240702Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTest120240702Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTest120240702Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/test/SpringbootTest120240702ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTest120240702ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
